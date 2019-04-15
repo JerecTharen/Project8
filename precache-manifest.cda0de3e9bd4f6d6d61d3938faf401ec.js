73c4b3b9aefddfdc7798225461926256f9c6424c
JerecTharen <kf7ags@gmail.com>
2019-04-15 09:39:48 -0600

    Updates

---
+++ precache-manifest.cda0de3e9bd4f6d6d61d3938faf401ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f14c0e180cf8e1d0440",
+    "url": "/Project8/static/js/runtime~main.f658002a.js"
+  },
+  {
+    "revision": "3f1cd53e2ed0b3b26c4d",
+    "url": "/Project8/static/js/main.896859f6.chunk.js"
+  },
+  {
+    "revision": "439a1806a5d627dd599a",
+    "url": "/Project8/static/js/2.bc70e7d4.chunk.js"
+  },
+  {
+    "revision": "3f1cd53e2ed0b3b26c4d",
+    "url": "/Project8/static/css/main.f189d015.chunk.css"
+  },
+  {
+    "revision": "e5b8ca0be7fb491600f44a8315f5d0b5",
+    "url": "/Project8/index.html"
+  }
+];
